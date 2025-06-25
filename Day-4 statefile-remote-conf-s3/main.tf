b3e85ac2f59e9369358c6decc8b9944fe6ed56ce
senjaliyadhruv <senjaliyadhruvr@gmail.com>
2025-06-25 23:10:15 +0530

    day 4

--- Day-4 statefile-remote-conf-s3/main.tf
+++ Day-4 statefile-remote-conf-s3/main.tf
@@ -1,13 +1,22 @@
 resource "aws_vpc" "dev-vpc" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "dev-vpc"
+  }
 }
 resource "aws_subnet" "subnet-1" {
   cidr_block = "10.0.0.0/24"
   vpc_id     = aws_vpc.dev-vpc.id
+  tags = {
+    Name = "subnet-1"
+  }
 
 }
 resource "aws_subnet" "subnet-2" {
-  cidr_block = "10.1.0.0/24"
+  cidr_block = "10.0.1.0/24"
   vpc_id     = aws_vpc.dev-vpc.id
+  tags = {
+    Name = "subnet-2"
+  }
 
 }
