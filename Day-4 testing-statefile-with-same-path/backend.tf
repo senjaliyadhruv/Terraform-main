20258e4501c47935a402a68f96ba1ad60c0a6f7a
senjaliyadhruv <senjaliyadhruvr@gmail.com>
2025-06-25 23:43:07 +0530

    same path

---
+++ Day-4 testing-statefile-with-same-path/backend.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "s3" {
+    bucket = "bucketforbackendstatefile"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+// if we keep same path for backend.tf file, it will override the previous state file
+// so we need to change the path of backend.tf file
+// or we can change the bucket name in backend.tf file
+// it will delete the resources which are existing in the previous state file
+// and create new resources in the new state file
+
+//we can change the path or name of the bucket
+
+
+terraform {
+  backend "s3" {
+    bucket = "bucketforbackendstatefile"
+    key    = "day-4/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+//this will create a new state file in the day-4 folder
+// and will not override the previous state file
