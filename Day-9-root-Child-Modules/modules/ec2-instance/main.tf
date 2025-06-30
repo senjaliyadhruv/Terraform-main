828c111342d8b61ac4986ee78dd7d3af47d2e5cd
senjaliyadhruv <senjaliyadhruvr@gmail.com>
2025-06-30 23:07:51 +0530

    day-9

---
+++ Day-9-root-Child-Modules/modules/ec2-instance/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+
+}
+module "web_instance" {
+  source        = "./modules/ec2-instance"
+  ami_id        = "ami-05ffe3c48a9991133"
+  instance_type = "t2.micro"
+  instance_name = "WebServer"
+
+}
