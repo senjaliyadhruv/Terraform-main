877ac800656d58c071a1d6af793e25152c214c28
senjaliyadhruv <senjaliyadhruvr@gmail.com>
2025-07-02 17:37:27 +0530

    Day-7

--- Day-7-RDS/provider.tf
+++ Day-7-RDS/provider.tf
@@ -1,9 +1,3 @@
 provider "aws" {
-  alias  = "primary"
-  region = "us-east-1" # Source region (Mumbai)
-}
-
-provider "aws" {
-  #   alias  = "secondary"
-  region = "us-west-2" # Destination region (Oregon)
+  
 }
