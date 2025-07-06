aa3355191afe955d8a32e6afdc3620f3f0686823
senjaliyadhruv <senjaliyadhruvr@gmail.com>
2025-07-06 23:42:55 +0530

    Day-13

--- Day-13-count-foreach/count/main.tf
+++ Day-13-count-foreach/count/main.tf
@@ -21,6 +21,7 @@ resource "aws_instance" "name" {
 #   default = ["dev", "test", "prod"] # # ["dev", "test", "prod"] when we delete "test" then prod will delete and test name will be modified as prod.
 # }
 
+
 # resource "aws_instance" "name" {
 #   ami           = "ami-085ad6ae776d8f09c"
 #   instance_type = "t2.micro"
