10af5b8ea0d643104027df7842adee1d905ff828
senjaliyadhruv <senjaliyadhruvr@gmail.com>
2025-07-10 16:15:59 +0530

    Day-14

--- Day-14-Conditions/main.tf
+++ Day-14-Conditions/main.tf
@@ -48,22 +48,22 @@
 # example-4 with numeric condition in third condition if ec2 instance = t2.micro only instance will create(count = var.instance_type == "t2.micro" ? 1 : 0) but i am passing t2.nano so ec2 will not create
 # variables.tf
 
-variable "ami" {
-  type    = string
-  default = "ami-0230bd60aa48260c6"
-}
+# variable "ami" {
+#   type    = string
+#   default = "ami-0230bd60aa48260c6"
+# }
 
-variable "instance_type" {
-  type    = string
-  default = "t2.nano"
-}
+# variable "instance_type" {
+#   type    = string
+#   default = "t2.nano"
+# }
 
-# main.tf
-resource "aws_instance" "dev" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  count         = var.instance_type == "t2.micro" ? 1 : 0
-  tags = {
-    Name = "dev_server"
-  }
-}
+# # main.tf
+# resource "aws_instance" "dev" {
+#   ami           = var.ami
+#   instance_type = var.instance_type
+#   count         = var.instance_type == "t2.micro" ? 1 : 0
+#   tags = {
+#     Name = "dev_server"
+#   }
+# }
