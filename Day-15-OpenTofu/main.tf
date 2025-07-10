6c3061d1010b08e9ca1a6364f1a5c089e4a9672b
senjaliyadhruv <senjaliyadhruvr@gmail.com>
2025-07-10 22:37:16 +0530

    Day-15

---
+++ Day-15-OpenTofu/main.tf
@@ -0,0 +1,7 @@
+#OpenTofu is the alternate tool of Terraform.
+#fully Open source IaC tool.
+
+resource "aws_instance" "question" {
+  ami           = "ami-00a929b66ed6e0de6"
+  instance_type = "t2.micro"
+}
