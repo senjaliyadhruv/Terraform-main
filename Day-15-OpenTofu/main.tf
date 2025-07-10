a51fa31d8294120d024abf36967232cf6228b1cb
senjaliyadhruv <senjaliyadhruvr@gmail.com>
2025-07-10 22:39:43 +0530

    Day-15-OpenTofu

--- Day-15-OpenTofu/main.tf
+++ Day-15-OpenTofu/main.tf
@@ -2,6 +2,7 @@
 #fully Open source IaC tool.
 #all command are same 
 #only changes are like tofu instead of terraform
+# terraform init --> tofu init
 resource "aws_instance" "question" {
   ami           = "ami-00a929b66ed6e0de6"
   instance_type = "t2.micro"
